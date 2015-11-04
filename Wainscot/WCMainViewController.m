36b2b26995f58f761a1a6c5cfd0fdd2f9600d2e2
jeffaburt <jburt1992@gmail.com>
2015-11-03 23:07:42 -0500

    Added basic UI components to WCMainVC and hooked them up

--- Wainscot/WCMainViewController.m
+++ Wainscot/WCMainViewController.m
@@ -7,8 +7,24 @@
 //
 
 #import "WCMainViewController.h"
+#import "WCPencilMarkCollectionViewCell.h"
+#import "WCComputedMeasurement.h"
+#import "WCFraction.h"
+#import "NSString+WCUnits.h"
 
-@interface WCMainViewController ()
+static CGFloat const kWCSegmentedControlOptionValueZero = 3.5f;
+static CGFloat const kWCSegmentedControlOptionValueOne = 4.125f;
+
+@interface WCMainViewController () <UICollectionViewDataSource, UICollectionViewDelegate, UITextFieldDelegate>
+
+@property (nonatomic, weak) IBOutlet UICollectionView *collectionView;
+@property (weak, nonatomic) IBOutlet UITextField *totalLengthTextField;
+@property (weak, nonatomic) IBOutlet UISegmentedControl *leftPostWidthSegmentedControl;
+@property (weak, nonatomic) IBOutlet UISegmentedControl *rightPostWidthSegmentedControl;
+@property (weak, nonatomic) IBOutlet UILabel *innerSpacingLabel;
+@property (weak, nonatomic) IBOutlet UILabel *numberOfInnerPostsLabel;
+
+@property (nonatomic) WCComputedMeasurement *computedMeasurement;
 
 @end
 
@@ -16,12 +32,81 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    [self refreshData];
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+#pragma mark - Lazy loading
+
+- (WCComputedMeasurement *)computedMeasurement {
+    if (!_computedMeasurement) {
+        _computedMeasurement =
+        [WCComputedMeasurement
+         computedMeasurementForTotalLength:self.totalLengthTextField.text.floatValue
+         leftPadding:[self selectedPaddingForSegmentedControl:self.leftPostWidthSegmentedControl]
+         rightPadding:[self selectedPaddingForSegmentedControl:self.rightPostWidthSegmentedControl]];
+    }
+    
+    return _computedMeasurement;
+}
+
+#pragma mark - UICollectionViewDataSource
+
+- (NSInteger)numberOfSectionsInCollectionView:(UICollectionView *)collectionView {
+    return 1;
+}
+
+- (NSInteger)collectionView:(UICollectionView *)collectionView
+     numberOfItemsInSection:(NSInteger)section {
+    return self.computedMeasurement.pencilMarks.count;
+}
+
+- (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView
+                  cellForItemAtIndexPath:(NSIndexPath *)indexPath {
+    WCPencilMarkCollectionViewCell *cell =
+    [collectionView dequeueReusableCellWithReuseIdentifier:NSStringFromClass([WCPencilMarkCollectionViewCell class])
+                                              forIndexPath:indexPath];
+    
+    cell.pencilMark = self.computedMeasurement.pencilMarks[indexPath.row];
+    
+    return cell;
+}
+
+#pragma mark - UITextFieldDelegate
+
+- (BOOL)textField:(UITextField *)textField
+shouldChangeCharactersInRange:(NSRange)range
+replacementString:(NSString *)string {
+    textField.text = [textField.text stringByReplacingCharactersInRange:range
+                                                             withString:string];
+    
+    [self refreshData];
+    
+    return NO;
+}
+
+- (IBAction)segmentedControlValueDidChange:(UISegmentedControl *)segmentedControl {
+    [self refreshData];
+}
+
+#pragma mark - Internal
+
+- (void)refreshData {
+    // Recompute the measurement
+    self.computedMeasurement = nil;
+    
+    self.innerSpacingLabel.text = [WCFraction fractionStringFromFloatValue:self.computedMeasurement.spacing].inchesString;
+    self.numberOfInnerPostsLabel.text = @(self.computedMeasurement.numberOfPosts).stringValue;
+    
+    [self.collectionView reloadData];
+}
+
+/** Returns the padding value for the given 'segmentedControl'. */
+- (CGFloat)selectedPaddingForSegmentedControl:(UISegmentedControl *)segmentedControl {
+    return
+    segmentedControl.selectedSegmentIndex == 0 ?
+    kWCSegmentedControlOptionValueZero :
+    kWCSegmentedControlOptionValueOne;
 }
 
 @end
