5d90a0b154dcc581c865fc45518828e72159be66
jeffaburt <jburt1992@gmail.com>
2015-11-09 08:58:20 -0500

    Round fractions to the nearest 16th, mimicking what's available on a tape measure

--- Wainscot/WCFraction.m
+++ Wainscot/WCFraction.m
@@ -9,12 +9,25 @@
 #import "WCFraction.h"
 
 static double const kWCFractionAcceptableError = 0.001f;
+static double const kWCFractionRoundedToDenominator = 16.0f;
 
 @implementation WCFraction
 
 + (NSString *)fractionStringFromFloatValue:(CGFloat)floatValue {
+    return [self fractionStringFromFloatValue:floatValue shouldRound:YES];
+}
+
+#pragma mark - Internal
+
++ (NSString *)fractionStringFromFloatValue:(CGFloat)floatValue
+                               shouldRound:(BOOL)shouldRound {
+    CGFloat roundedFloatValue =
+    shouldRound ?
+    round(floatValue * kWCFractionRoundedToDenominator) /
+    kWCFractionRoundedToDenominator : floatValue;
+    
     NSDecimalNumber *decimalNumber =
-    [NSDecimalNumber decimalNumberWithString:@(floatValue).stringValue];
+    [NSDecimalNumber decimalNumberWithString:@(roundedFloatValue).stringValue];
     
     WCFraction *fraction = [self fractionWithDecimalNumber:decimalNumber
                                            acceptableError:kWCFractionAcceptableError];
--- WainscotTests/WCFractionSpec.m
+++ WainscotTests/WCFractionSpec.m
@@ -9,27 +9,51 @@
 #import <Kiwi/Kiwi.h>
 #import "WCFraction.h"
 
+@interface WCFraction ()
+
++ (NSString *)fractionStringFromFloatValue:(CGFloat)floatValue
+                               shouldRound:(BOOL)shouldRound;
+
+@end
+
 SPEC_BEGIN(WCFractionSpec)
 
 describe(@"WCFraction", ^{
     
     describe(@"#fractionWithDecimalNumber:acceptableError:", ^{
+        __block BOOL shouldRound;
         
-        it(@"returns 1/3 when .333333333", ^{
-            [[[WCFraction fractionStringFromFloatValue:0.333333333f] should] equal:@"1/3"];
+        context(@"not rounded", ^{
+            
+            beforeEach(^{
+                shouldRound = NO;
+            });
+            
+            it(@"returns 1/3 when .333333333", ^{
+                [[[WCFraction fractionStringFromFloatValue:0.333333333f shouldRound:shouldRound] should]
+                 equal:@"1/3"];
+            });
+            
+            it(@"returns 1/3 when .333", ^{
+                [[[WCFraction fractionStringFromFloatValue:0.333f shouldRound:shouldRound] should]
+                 equal:@"1/3"];
+            });
+            
+            it(@"returns 33/100 when .33", ^{
+                [[[WCFraction fractionStringFromFloatValue:0.33f shouldRound:shouldRound] should]
+                 equal:@"33/100"];
+            });
+            
+            it(@"returns 3/10 when .3", ^{
+                [[[WCFraction fractionStringFromFloatValue:0.3f shouldRound:shouldRound] should]
+                 equal:@"3/10"];
+            });
+            
         });
         
-        it(@"returns 1/3 when .333", ^{
-            [[[WCFraction fractionStringFromFloatValue:0.333f] should] equal:@"1/3"];
-        });
-        
-        it(@"returns 33/100 when .33", ^{
-            [[[WCFraction fractionStringFromFloatValue:0.33f] should] equal:@"33/100"];
-        });
-        
-        it(@"returns 3/10 when .3", ^{
-            [[[WCFraction fractionStringFromFloatValue:0.3f] should] equal:@"3/10"];
-        });
+    });
+    
+    describe(@"#fractionWithDecimalNumber:acceptableError:", ^{
         
         it(@"returns 4 1/8 when 4.125", ^{
             [[[WCFraction fractionStringFromFloatValue:4.125f] should] equal:@"4 1/8"];
@@ -41,6 +65,18 @@ describe(@"WCFraction", ^{
         
         it(@"returns 0 when 0", ^{
             [[[WCFraction fractionStringFromFloatValue:0] should] equal:@"0"];
+        });
+        
+        it(@"rounds up to the nearest 16th", ^{
+            [[[WCFraction fractionStringFromFloatValue:1.0f / 32.0f] should] equal:@"1/16"];
+        });
+        
+        it(@"does not round when already a 16th and cannot be reduced", ^{
+            [[[WCFraction fractionStringFromFloatValue:3.0f / 16.0f] should] equal:@"3/16"];
+        });
+        
+        it(@"reduces 2/16 to 1/8", ^{
+            [[[WCFraction fractionStringFromFloatValue:2.0f / 16.0f] should] equal:@"1/8"];
         });
         
     });
